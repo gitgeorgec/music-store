1e3ff23c79cea2c15ba25d67ea2417e07ea35228
george <ccchang0661@gmail.com>
2018-11-02 20:36:48 +0800

    Updates

---
+++ precache-manifest.24b15e526e27b538c34ad09977b7e018.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "331197b514b61dd033bd153d28e1c51d",
+    "url": "/music-store/static/media/malte-wingen-381988-unsplash.331197b5.jpg"
+  },
+  {
+    "revision": "a21982e69337c026fe4ac4fad2821e3e",
+    "url": "/music-store/static/media/anthony-delanoix-15928-unsplash2.a21982e6.jpg"
+  },
+  {
+    "revision": "8488dd5416f04934094b",
+    "url": "/music-store/static/js/runtime~main.8488dd54.js"
+  },
+  {
+    "revision": "870b03c711e12b75e3b6",
+    "url": "/music-store/static/js/main.870b03c7.chunk.js"
+  },
+  {
+    "revision": "33736d30de3244d8ab0d",
+    "url": "/music-store/static/js/1.33736d30.chunk.js"
+  },
+  {
+    "revision": "870b03c711e12b75e3b6",
+    "url": "/music-store/static/css/main.6be7aaed.chunk.css"
+  },
+  {
+    "revision": "28d939d03df9791ffe887d6a94e67b14",
+    "url": "/music-store/index.html"
+  }
+];
