f857fccf411d5f639c67a779961b3e2647c25af9
george <ccchang0661@gmail.com>
2018-11-06 22:10:15 +0800

    Updates

--- precache-manifest.42acc20df9d10da5f440650d4c84db13.js
+++ precache-manifest.42acc20df9d10da5f440650d4c84db13.js
@@ -8,23 +8,27 @@ self.__precacheManifest = [
     "url": "/music-store/static/media/anthony-delanoix-15928-unsplash2.a21982e6.jpg"
   },
   {
+    "revision": "11dc437ab8ef0c32289c092083d20068",
+    "url": "/music-store/static/media/Spotify_Logo_RGB_Green.11dc437a.png"
+  },
+  {
     "revision": "8488dd5416f04934094b",
     "url": "/music-store/static/js/runtime~main.8488dd54.js"
   },
   {
-    "revision": "ad34178e42992d09ff8e",
-    "url": "/music-store/static/js/main.ad34178e.chunk.js"
+    "revision": "3a3f7a89ac2658371bd4",
+    "url": "/music-store/static/js/main.3a3f7a89.chunk.js"
   },
   {
-    "revision": "33736d30de3244d8ab0d",
-    "url": "/music-store/static/js/1.33736d30.chunk.js"
+    "revision": "e92c82753378606e1171",
+    "url": "/music-store/static/js/1.e92c8275.chunk.js"
   },
   {
-    "revision": "ad34178e42992d09ff8e",
-    "url": "/music-store/static/css/main.6be7aaed.chunk.css"
+    "revision": "3a3f7a89ac2658371bd4",
+    "url": "/music-store/static/css/main.b34cc975.chunk.css"
   },
   {
-    "revision": "190f2515b50df4f4e1d33132d3d6e12f",
+    "revision": "0822c71c04ce42621770026840cbaf19",
     "url": "/music-store/index.html"
   }
 ];
